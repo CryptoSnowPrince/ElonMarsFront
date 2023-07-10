e4d05b62abdec423cffebb5e838dd710fed1ba4e
CryptoSnowPrince <topdirector2017@gmail.com>
2023-07-11 03:45:07 +0900

    update config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,10 @@
 const config = {
     // websiteURL: "http://localhost:3001", // Test
     // server: 'http://localhost', // Test
-    websiteURL: "https://play.elonmars.io",
-    server: 'https://api.play.elonmars.io',
-    port: '',
+    // port: '8443', // Test
+    websiteURL: "https://play.elonmars.io", // Main
+    server: 'https://api.play.elonmars.io', // Main
+    port: '', // Main
     baseURL: '/api/v1',
     appID: 'elonmars-game',
     agentUID: 'elonmars',
